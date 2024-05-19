113c803e6dc9f776fc8f64eae3eab97ec4c93bb5
Julio Foulquie <1239921+j3j5@users.noreply.github.com>
2024-05-19 17:40:46 -0300

    Schedule check-on-date command for 'yesterday' every day at 10:00AM Uruguay time

--- routes/console.php
+++ routes/console.php
@@ -2,7 +2,10 @@
 
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
+use Illuminate\Support\Facades\Schedule;
 
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote')->hourly();
+
+Schedule::command('php artisan iNat:check-on-date yesterday')->dailyAt('10:00');
